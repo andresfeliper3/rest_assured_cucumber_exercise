29e5df08ff95e0463640a8647514a07e6f0cebac
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-03-08 18:09:10 -0500

    test case 3 in progress

--- src/test/java/com/testing/api/stepDefinitions/ClientSteps.java
+++ src/test/java/com/testing/api/stepDefinitions/ClientSteps.java
@@ -2,6 +2,7 @@ package com.testing.api.stepDefinitions;
 
 import com.testing.api.models.Client;
 import com.testing.api.requests.ClientRequest;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 
 import java.util.List;
+import java.util.Map;
 
 public class ClientSteps {
     private static final Logger logger = LogManager.getLogger(ClientSteps.class);
@@ -40,14 +42,37 @@ public class ClientSteps {
         Assert.assertTrue(clientList.size() >= requiredClientsAmount);
     }
 
+    @Given("I have a client with the following details:")
+    public void iHaveAClientWithTheFollowingDetails(DataTable expectedData) {
+        List<Map<String, String>> clientMap = expectedData.asMaps(String.class, String.class);
+        client = Client.builder()
+                .name(clientMap.get(0).get("Name"))
+                .lastName(clientMap.get(0).get("LastName"))
+                .country(clientMap.get(0).get("Country"))
+                .city(clientMap.get(0).get("City"))
+                .email(clientMap.get(0).get("Email"))
+                .phone(clientMap.get(0).get("Phone"))
+                .build();
+    }
+
     @When("I send a GET request to retrieve all the list of clients")
     public void iSendAGETRequestToRetrieveAllTheListOfClients() {
         response = clientRequest.getAll();
-        logger.info("Status code is: " + response.statusCode());
+    }
+
+    @When("I send a POST request to create a client")
+    public void iSendAPOSTRequestToCreateAClient() {
+        response = clientRequest.createClient(client);
     }
 
     @Then("the clients response should have a status code of {int}")
     public void theClientsResponseShouldHaveAStatusCodeOf(int statusCode) {
+        Assert.assertEquals(statusCode, response.statusCode());
+    }
+
+    @Then("the new client request should have a status code of {int}")
+    public void theNewClientRequestShouldHaveAStatusCodeOf(int statusCode) {
+        logger.info("Status code is: " + response.statusCode());
         Assert.assertEquals(statusCode, response.statusCode());
     }
 
@@ -57,4 +82,22 @@ public class ClientSteps {
         Assert.assertTrue(clientRequest.validateSchema(response, path));
         logger.info("Schema from clients list was validated");
     }
+
+
+    @And("the response should include the details of the created client")
+    public void theResponseShouldIncludeTheDetailsOfTheCreatedClient() {
+        Client returnedClient = clientRequest.getClientEntity(response);
+        Assert.assertEquals(client.getName(), returnedClient.getName());
+        //complete this
+    }
+
+    @And("validates the new client response with the client JSON schema")
+    public void validatesTheNewClientResponseWithTheClientJSONSchema() {
+        String path = "schemas/clientSchema.json";
+        Assert.assertTrue(clientRequest.validateSchema(response, path));
+        logger.info("Schema from client creation response was validated");
+    }
+
+
+
 }
--- src/test/resources/features/client_testing_crud.feature
+++ src/test/resources/features/client_testing_crud.feature
@@ -14,3 +14,13 @@ Feature: Client testing CRUD
     When I send a GET request to retrieve all the list of resources
     Then the resources response should have a status code of 200
     And validates the response with resource list JSON schema
+
+  @smoke
+  Scenario: Create a new client
+  Given I have a client with the following details:
+    | Name | LastName | Country | City | Email | Phone |
+    | Andres | Rincon | Colombia | Cali | andres@gmail.com | 3157642345 |
+  When I send a POST request to create a client
+  Then the new client request should have a status code of 201
+  And the response should include the details of the created client
+  And validates the new client response with the client JSON schema
